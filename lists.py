f1ca31b6acdde73e72cbb14e98a8805911d2845a
shireeshavidem <shireesha.marolix@gmail.com>
2023-10-09 17:40:29 +0530

    first commit

---
+++ lists.py
@@ -0,0 +1,66 @@
+# li = []
+# n = int(input("Enter length of the lenth: "))
+# for i in range(0,n):
+#    li1 = input("Enter your elememts: ")
+#    li.append(li1)
+# ele = input("enter element you want to check: ")   
+# if ele in li:
+#         print("true")
+# else:
+#         print("false")    
+
+
+# flowers = []
+# colors = []
+# n = int(input("Enter length of the lenth: "))
+# for i in range(0,n):
+#     flowers1 = input("Enter the flower name: ")
+#     color1 = input("Enter the flowers color: ")
+#     flowers.append(flowers1)
+#     colors.append(color1)
+# z = zip(flowers,colors)
+# for flowers,colors in z:
+#     print("%s is in %s color" %(flowers,colors))
+
+
+#interchanging first and last element in list
+list = []
+n = int(input("Enter length of the lenth: "))
+#1
+# def interchangelist(list):
+for i in range(0,n):
+    list1 = input("Enter your elements: ")
+    list.append(list1)
+#     size = len(list)    
+#     temp = list[0]
+#     list[0] = list[size-1]
+#     list[size-1] = temp
+#     return list
+# print(interchangelist(list))
+
+#2
+# def change(list):
+#     list[0],list[-1] = list[-1],list[0]
+#     return list
+# print(change(list))
+
+#3
+# def change(list):
+#     get = list[0],list[-1]
+#     list[-1],list[0] = get
+#     return list
+# print(change(list))
+
+#4
+# def change(list):
+#     start,*middle,end = list
+#     list = end,*middle,start
+#     return list
+# print(change (list)) 
+
+#5
+def interchangelist(list):
+    if len(list) >= 2:
+        list = list[-1:] + list[1:-1] + list[:1]
+    return list
+print(interchangelist(list))    
