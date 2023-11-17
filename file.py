f7d4905b3f00d34c7f91ccd206da20fcdeaaf88d
shireeshavidem <shireesha.marolix@gmail.com>
2023-11-17 18:00:04 +0530

    files

---
+++ file.py
@@ -0,0 +1,21 @@
+# lst = [1,2,1,3,4,1]
+# lst2 = []
+# for i in lst:
+#     if i not in lst2:
+#         lst2.append(i)
+# print(lst2)  
+# k = input()
+dic = {1:2, 2:3}
+# for i in dic.keys():
+#     if k == i:
+#         print("already exists")
+#     else:
+#         print(k)    
+
+# a = [1,3,4,6,7,8]
+
+# a.sort()
+# a.reverse()
+# print(a[0],a[1])
+total_sum = sum(dic.values())
+print(total_sum)
---
+++ practice.py
@@ -0,0 +1,15 @@
+a=5
+# b=1
+# while b <= a:
+
+#     if (b % 2)== 0:
+#         print(b*b)
+#     b = (b + 1)    
+
+str = "marolix technology"
+# count = ''
+# for i in str:
+#     if str.count(i)==1:
+#         count+=i
+# print(count)
+print(str.replace(" ",""))        
