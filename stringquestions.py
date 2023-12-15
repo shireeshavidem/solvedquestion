5ed060058f3a47e646581d0c627260addc343faa
shireeshavidem <shireesha.marolix@gmail.com>
2023-12-15 18:03:01 +0530

    strings

---
+++ stringquestions.py
@@ -0,0 +1,23 @@
+# 1
+# string = input("Enter your string: ")
+# char = input("Enter the char: ")
+# if char in string:
+#     str=string.replace(char,"")
+#     print(str)
+# else: print("invalid char")
+
+# dict = {ord(char):None}
+# print(string.translate(dict))
+
+# 2
+# str = input("Enter a string: ")
+# char = input("Enter a char: ")
+# print(str.count(char))
+
+# 3
+# str1 = input("Enter string 1: ")
+# str2 = input("Enter string 2: ")
+# if sorted(str1)==sorted(str2):
+#     print("Two strings are Anagram")
+# else: print("Two strings are not Anagram: ")
+
